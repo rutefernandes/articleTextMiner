17cb4551201c1c651dd8eab22d452890449eb002
Tiago Lisboa <tiago.caio.ol@gmail.com>
2019-10-27 13:28:42 -0300

    melhoria nas captura de referências

--- articleTextMiner/src/main/java/atm/Teste.java
+++ articleTextMiner/src/main/java/atm/Teste.java
@@ -1,15 +1,18 @@
 package atm;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Teste {
 	public static void main(String[] args) throws IOException {
-		ArticleTextMiner i = new ArticleTextMiner("./data/Gravitational.pdf");
+		ArticleTextMiner i = new ArticleTextMiner("./data/template_ieee.pdf");
 		System.out.println(i.Title());
 		System.out.println(i.Abstract());
 		for (String a : i.Country()) {
 			System.out.println(a);
 		}
-		i.References();
+		System.out.println("REFERENCES");
+		System.out.println(i.References());
 	}
 }
