54bb7643fdcd3c3af3aa5206be2afae5db9ad367
Tiago Lisboa <tiago.caio.ol@gmail.com>
2019-10-27 10:51:35 -0300

    pronto pro webapp

--- articleTextMiner/src/main/java/atm/Teste.java
+++ articleTextMiner/src/main/java/atm/Teste.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 public class Teste {
 	public static void main(String[] args) throws IOException {
-		ArticleTextMiner i = new ArticleTextMiner("./data/games.pdf");
-		System.out.println(i.Keywords());
-		/*for (String a : i.Country()) {
+		ArticleTextMiner i = new ArticleTextMiner("./data/Gravitational.pdf");
+		System.out.println(i.Title());
+		System.out.println(i.Abstract());
+		for (String a : i.Country()) {
 			System.out.println(a);
-		} */ 
+		}
+		i.References();
 	}
 }
