884318cca73b10fe711b7387c81b64dfa6e2772b
Rute Fernandes <rutefdn@hotmail.com>
2019-12-18 13:39:45 -0300

    exemplos de arquivo

--- articleTextMiner/src/main/java/atm/Teste.java
+++ articleTextMiner/src/main/java/atm/Teste.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 
 public class Teste {
 	public static void main(String[] args) throws IOException {
-		ArticleTextMiner i = new ArticleTextMiner("./data/Gravitational.pdf");
+		String path = "./data/Gravitational.pdf";
+		ArticleTextMiner i = new ArticleTextMiner(path);
+		i.setUrlPath();
 		System.out.println(i.Title());
 		System.out.println(i.Abstract());
 		for (String a : i.Country()) {
